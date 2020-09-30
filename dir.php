be9a6ce08b44cb2a375db22edcaf11524d16d8a9
Tetsu Kondo <tetsu.kondo@int.t-kougei.ac.jp>
2020-09-30 09:05:42 +0900

    v
    v

--- dir.php
+++ dir.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>フォルダ表示PHP</title>
@@ -7,41 +8,16 @@
 <body>
 
 <?php
-    // 現在の相対パスを取得
-    $path = dirname(__FILE__) .'/';
- 
-    // ディレクトリ一覧の取得
-    $dirs = scandir($path);
- 
-    // 表示させないディレクトリ配列
-    $excludes = array(
-        '.',
-        '..',
-        'css',
-        'test',
-        'test2',
-    );
- 
-    // リンク表示
-    echo '<ul>',"\n";
-    foreach ($dirs AS $dir) {
- 
-        // 特定のディレクトリの場合は表示させない
-        if (in_array($dir, $excludes)) {
-            continue;
-        }
- 
-        if ((is_dir($dir) === true)) {
-            echo '<li>';
-            echo '<a href="./' . $dir . '">';
-            echo h($dir);
-            echo '</a></li>'."\n";
-        }
-    }
-    echo '</ul>',"\n";
-
-    ?>
-    
-
-    </body>
+  
+$path = dirname(__FILE__) .'/';
+$dirlist = dir($dirpath);
+while( $filename = $dirlist->read() ){
+	//ディレクトリの場合は表示対象外（＝ファイルのみ表示）
+	if( (is_dir($filename) == false) && ($filename!=".." || $filename!= "." ) ){
+		print("<a href=\"" . $dirpath . $filename . "\">".$filename."</a><br />\n");
+	}
+}
+$dirlist->close();
+?>
+</body>
 </html>
